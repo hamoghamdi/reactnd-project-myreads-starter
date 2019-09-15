dcc92a9623277b40f1c9a93f23c6fbc1d1650579
hamoghamdi <haneen.aljabrah@gmail.com>
2019-09-15 19:20:54 +0300

    functionality :view all books, move a book, view books shelf updated

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Route} from 'react-router-dom'
 import AddBooks from './AddBooks';
 import BooksShelf from './BooksShelf'
 
 class BooksApp extends React.Component {
-  state = {
-    books: []
-  };
-  moveBook = event => {
-    console.log("move book", event.target.value);
-  };
+
   /* ------ Comments ------ //
-   + BooksShelf => .filter .mapX3
    + Search Component => onSubmit
    + moveBook => API.update 
-// => COMPONENTS: App, BooksShelf( CurrentlyReading, WantoRead, Read)
 
   // ------ Comments ------ */
-
-  componentDidMount(){
-
-    console.log("this is component did mount")
-    BooksAPI.getAll()
-      .then(res => {
-        console.log("res", res)
-        this.setState(()=> {
-          return { books: res }
-        })
-      }
-        )
-      .catch(error => console.log("error", error))
-  }
   render() {
     return (
       <div className="app">
       <Route exact path="/add" render={()=>(<AddBooks />)} />
-      <Route exact path="/" render={()=> (<BooksShelf books={this.state.books}/>)} />
+      <Route exact path="/" render={()=> (<BooksShelf />)} />
       </div>
     );
   }
--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Book(props) {
+class Book extends Component {
+  handleChange = (e, book) =>{
+      this.props.onMoveBook(e, book);    
+    console.log('book component', book)
+  }
+  render() {
+   
     return (
-        <div>
-            
+            <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
+              }}
+            ></div>
+            <div className="book-shelf-changer">
+              <select onChange={(e) => this.handleChange(e, this.props.book)} >
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{this.props.book.title}</div>
+          <div className="book-authors">
+            {this.props.book.authors.map((a, index) => (
+              <p key={index}>{a}</p>
+            ))}
+          </div>
         </div>
+      </li>
     );
+  }
+}
 
-    }
 export default Book;
+
+// onChange={e => this.moveBook(e)}
